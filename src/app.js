aa059aebcd26d42369a0b6bfef06b1d23661b4c9
Josue Hernandez <moradel82@gmail.com>
2021-01-23 15:52:38 -0700

    Modified app files for deployment

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const helmet = require("helmet");
 const app = express();
 const { NODE_ENV } = require("./config");
 
-const morganOption = NODE_ENV === "production";
+const morganOption = NODE_ENV === "production" ? 'tiny' : 'common';
 
 app.use(morgan(morganOption));
 app.use(helmet());
